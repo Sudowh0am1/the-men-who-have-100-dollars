72b70121abcf984dbffe5a8aaed8d91d21c671e8
Your Name <a80ataee@gmail.com>
2021-08-22 16:32:09 +0430

    comments to source code added

--- the men who have 100 dollars.py
+++ the men who have 100 dollars.py
@@ -1,10 +1,10 @@
-# this is the Question : there's 50 men who each one has 100 dollar. how much money will remain for each one
-# if in every round  they borrow exactly 1 dollar to a random person.
-# suppose that we do it for 200 rounds. POINT : the person who has zero money will be omitted. 
+# this is the Question : there's 50 men and each one has 100 dollar. how much money will remain for each one
+# if in every round they borrow exactly 1 dollar to a random person.
+# suppose that we do it 200 times. POINT : the one who has zero money will be omitted. 
 
 
 import random
-#import Matplotlib.pyplot as plt
+# import Matplotlib.pyplot as plt
 
 peoples_money = []
 for i in range (0, 50):
