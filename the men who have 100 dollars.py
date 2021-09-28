21568ce889ad81587b1d5d685296ea0b0fab6ad1
Sudowh0am1 <89195171+Sudowh0am1@users.noreply.github.com>
2021-09-28 20:59:16 +0330

    Update the men who have 100 dollars.py

--- the men who have 100 dollars.py
+++ the men who have 100 dollars.py
@@ -1,4 +1,4 @@
-# this is the Question : there's 50 men and each one has 100 dollar. how much money will remain for each one
+# here is the Question : there's 50 men and each one has 100 dollar. how much money will remain for each one
 # if in every round they borrow exactly 1 dollar to a random person.
 # suppose that we do it 200 times. POINT : the one who has zero money will be omitted. 
 
