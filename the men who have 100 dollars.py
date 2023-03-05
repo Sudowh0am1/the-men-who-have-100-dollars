d9fb21762133ae1e5e8601efbb056bc90e054d29
Sudowh0am1 <89195171+Sudowh0am1@users.noreply.github.com>
2023-03-05 16:50:00 +0330

    Update the men who have 100 dollars.py

--- the men who have 100 dollars.py
+++ the men who have 100 dollars.py
@@ -1,7 +1,3 @@
-# here is the Question : there's 50 men and each one has 100 dollar. how much money will remain for each one
-# if in every round they borrow exactly 1 dollar to a random person.
-# suppose that we do it 200 times. POINT : the one who has zero money will be omitted. 
-
 
 import random
 # import Matplotlib.pyplot as plt
